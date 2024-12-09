bc0a2bc1476d7e7a9a82185a701c0b4a555f821f
colewortman <wortcole12@gmail.com>
2024-12-08 23:42:39 -0600

    Connecting and rendering the mock API

---
+++ src/components/Posts.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../services/PostService";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+      .then((result) => {
+        setPosts(result.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
