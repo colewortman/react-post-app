db831a05b75cc56e55514f66ce871ca7a2ad06f9
colewortman <wortcole12@gmail.com>
2024-12-13 15:59:20 -0600

    adding CRUD

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -5,5 +5,8 @@ const api = axios.create({
 });
 
 const getPosts = () => api.get("/posts");
+const deletePost = (id) => api.delete(`/posts/${id}`);
+const createPost = (post) => api.post("/posts", post);
+const updatePost = (id, post) => api.put(`/posts/${id}`, post);
 
-export { getPosts };
+export { getPosts, deletePost, createPost, updatePost };
